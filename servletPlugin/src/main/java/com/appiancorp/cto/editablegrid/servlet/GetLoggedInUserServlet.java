21758839b45982ab9f9961c5ac5d615d0947e8b7
alex-king10 <alex.king@appian.com>
2024-06-12 15:11:21 -0400

    String output to port working

--- servletPlugin/src/main/java/com/appiancorp/cto/editablegrid/servlet/GetLoggedInUserServlet.java
+++ servletPlugin/src/main/java/com/appiancorp/cto/editablegrid/servlet/GetLoggedInUserServlet.java
@@ -1,11 +1,11 @@
 package com.appiancorp.cto.editablegrid.servlet;
+// import com.appiancorp.suiteapi.security.external.SecureCredentialsStore;
 
-import com.appiancorp.suiteapi.process.ProcessDesignService;
-import com.appiancorp.suiteapi.security.external.SecureCredentialsStore;
+ import com.appiancorp.suiteapi.process.ProcessDesignService;
 import com.appiancorp.suiteapi.servlet.AppianServlet;
+// import com.appiancorp.suiteapi.type.TypedValue;
 
-
-import javax.servlet.ServletException;
+ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,14 +16,24 @@ import org.json.JSONObject;
 
 public class GetLoggedInUserServlet extends AppianServlet {
 //    private static final Logger LOG = LogManager.getLogger(GetLoggedInUserServlet.class);
-    ProcessDesignService pds;
-    SecureCredentialsStore scs;
+     ProcessDesignService pds;
+    // SecureCredentialsStore scs;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-//        PrintWriter out = resp.getWriter();
         JSONObject result = new JSONObject();
+        String financeUUID = "168e5b0c-8188-45be-be8a-588dcd20e84c";
+        try {
+            String getNameExpression = String.format("tostring(recordType!{%s})", financeUUID);
+//            TypedValue expressionResult = pds.evaluateExpression(getNameExpression);
+//            String ruleResult = expressionResult.getValue().toString();
+            result.put("Result", getNameExpression);
+
+        } catch (Exception e) {
+            result.put("Error", e.toString());
+        }
+
         result.put("yay", "it workin");
         result.put("success", true);
         resp.setContentType("application/json");
@@ -32,10 +42,6 @@ public class GetLoggedInUserServlet extends AppianServlet {
         out.write(result.toString());
         out.flush();
         out.close();
-
-//        out.print(result);
-//        sendResponse(out, result);
-//        System.out.println("Servlet is working!");
     }
 
 //    public GetLoggedInUserServlet(ProcessDesignService pds, SecureCredentialsStore scs) {
