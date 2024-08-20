3f74d41a4efde1feae8d0dabeeff66a68e0fcd24
Brooks Watson <brooks.watson@appian.com>
2024-08-20 08:06:28 -0400

    Restoring spacing

--- componentPlugin/editableGridComponentField/v1/components/GridComponent.js
+++ componentPlugin/editableGridComponentField/v1/components/GridComponent.js
@@ -1,230 +1,184 @@
-import { formatColumnHeader } from "../services/parameters.js";
+import {
+  formatColumnHeader
+} from "../services/parameters.js"
 
 class GridComponent {
 
-    // init grid component instance
-    constructor(containerId, data, columnConfigs, gridOptions, editablePKFieldList, hotInstance, userPermissionLevel) {
-        this.containerId = containerId;
-        this.data = data;
-        this.columnConfigs = columnConfigs;
-        this.gridOptions = gridOptions;
-        this.editablePKFieldList = editablePKFieldList;
-        this.changeObj = {};
-        // typically not set on instantiation of object
-        this.hotInstance = hotInstance;
-        this.userPermissionLevel = userPermissionLevel;
-    }
+  // init grid component instance
+  constructor(containerId, data, columnConfigs, gridOptions, editablePKFieldList, hotInstance, userPermissionLevel) {
+      this.containerId = containerId;
+      this.data = data;
+      this.columnConfigs = columnConfigs;
+      this.gridOptions = gridOptions;
+      this.editablePKFieldList = editablePKFieldList;
+      this.changeObj = {};
+      // typically not set on instantiation of object
+      this.hotInstance = hotInstance;
+      this.userPermissionLevel = userPermissionLevel;
+  }
 
   // initialize grid instance
   initGrid() {
-    const container = document.getElementById(this.containerId);
-    if (!container) {
-      console.error("Grid container does not exist");
-    }
-
-    this.hotInstance = new Handsontable(container, {
-      licenseKey: "non-commercial-and-evaluation",
-    });
-
-    const testValidator = (query, callback) => {
-      let isValid = false;
-
-      isValid = query === "UNATTENDED";
-
-      callback(isValid);
-    };
-
-    this.columnConfigs.forEach((colConfig, index) => {
-      if (colConfig.validator) {
-        const { name, operator, value } = colConfig.validator;
-
-        const customValidator = (query, callback) => {
-          let isValid = false;
-
-          switch (operator) {
-            case "equals":
-              isValid = query === value;
-              break;
-            case "greaterThan":
-              isValid = query > value;
-              break;
-            case "lessThan":
-              isValid = query < value;
-              break;
-            default:
-              console.error("Unknown operator:", operator);
-          }
-
-          callback(isValid);
-        };
-
-        Handsontable.validators.registerValidator(name, customValidator);
-
-        colConfig.validator = name;
+      const container = document.getElementById(this.containerId);
+      if (!container) {
+          console.error('Grid container does not exist');
       }
-    });
 
-    this.hotInstance.updateSettings({
-      data: this.data,
-      columns: this.columnConfigs,
-    });
+      this.hotInstance = new Handsontable(container, {
+          licenseKey: "non-commercial-and-evaluation",
+        });
+      
+      this.hotInstance.updateSettings({
+          data: this.data,
+          columns: this.columnConfigs,
+      });
 
-    this.setGridOptions(this.gridOptions);
+      this.setGridOptions(this.gridOptions);
 
-    this.addListeners();
+      this.addListeners();
   }
 
   setData(data) {
-    // write this logic
-    this.data = data;
-    this.hotInstance.updateSettings({
-      data: this.data,
-    });
+      // write this logic
+      this.data = data;
+      this.hotInstance.updateSettings({
+          data: this.data,
+      });
   }
 
   setColumnConfigs(columnConfigs) {
-    this.columnConfigs = columnConfigs;
-
-    this.columnConfigs.forEach((colConfig, index) => {
-      if (index === 0) {
-        colConfig.validator = "test";
-      }
-    });
+      this.columnConfigs = columnConfigs;
+      this.hotInstance.updateSettings({
+          columns: this.columnConfigs,
+      });
   }
 
-  // TODO
-  validateColumns(columnsToValidate) {
-    this.hotInstance.validateColumns(columnsToValidate, (valid) => {
-      console.log(valid);
-    });
-  }
 
   // not sure if this function logic should go here?
   updateData() {
-    let currRowIdx;
-    let currChangeItem;
+      let currRowIdx;
+      let currChangeItem;
 
-    // console.log("Updating data");
+      // console.log("Updating data");
 
-    if (this.data != null && this.data.length != 0) {
-      if (Object.keys(this.changeObj).length == 0) {
-        // no updates to make to data var
-        return this.data;
-      } else {
-        for (let i = 0; i < Object.keys(this.changeObj).length; i++) {
-          currRowIdx = Object.keys(this.changeObj)[i];
-          currChangeItem = Object.values(this.changeObj)[i];
-          // update row in data var with value from changeObj
-          this.data[currRowIdx] = Object.assign(
-            this.data[currRowIdx],
-            currChangeItem
-          );
-        }
+      if (this.data != null && this.data.length != 0) {
+          if (Object.keys(this.changeObj).length == 0) {
+              // no updates to make to data var
+              return this.data;
+          } else {
+              for (let i = 0; i < Object.keys(this.changeObj).length; i++) {
+                  currRowIdx = Object.keys(this.changeObj)[i];
+                  currChangeItem = Object.values(this.changeObj)[i];
+                  // update row in data var with value from changeObj
+                  this.data[currRowIdx] = Object.assign(this.data[currRowIdx], currChangeItem);
+              }
+          }
       }
-    }
   }
 
   setGridOptions(gridOptions) {
-    this.gridOptions = gridOptions;
 
-    this.hotInstance.updateSettings(this.gridOptions);
+      this.gridOptions = gridOptions;
 
-    // handle column header formatting on sort
-    this.hotInstance.updateSettings({
-      afterGetColHeader: function (column, TH) {
-        if (column > -1) {
-          formatColumnHeader(TH);
-        }
-      },
-    });
+      this.hotInstance.updateSettings(this.gridOptions);
+
+      // handle column header formatting on sort
+      this.hotInstance.updateSettings({
+          afterGetColHeader: function(column, TH) {
+              if (column > -1) { formatColumnHeader(TH); } 
+          }
+      });
   }
 
   getGridOptions() {
-    return this.gridOptions;
+      return this.gridOptions;
   }
 
-    setChangeObj(changeObj) {
-        this.changeObj = changeObj;
-    }
+  setChangeObj(changeObj) {
+      this.changeObj = changeObj;
+  }
 
-    getUserPermissionLevel() {
-        return this.userPermissionLevel;
-    }
+  getUserPermissionLevel() {
+      return this.userPermissionLevel;
+  }
 
-    setUserPermissionLevel(userPermissionLevel) {
-        this.userPermissionLevel = userPermissionLevel;
-    }
+  setUserPermissionLevel(userPermissionLevel) {
+      this.userPermissionLevel = userPermissionLevel;
+  }
 
-    // Handles changes made to the grid by modifying this.changeObj
-    // Called by addListeners in 'afterChange' hook
-    onChange(cellMeta, newValue) {
-        if (cellMeta != null) {
-            let dataItem = {};
-            let gridRow;
+  // Handles changes made to the grid by modifying this.changeObj
+  // Called by addListeners in 'afterChange' hook
+  onChange(cellMeta, newValue) {
+      if (cellMeta != null) {
+          let dataItem = {};
+          let gridRow;
 
-      if (cellMeta.row in this.changeObj) {
-        this.changeObj[cellMeta.row][cellMeta.prop] = newValue;
-      } else {
-        // add PK if exists
-        // access data at modified row
-        gridRow = this.data[cellMeta.row];
+          if (cellMeta.row in this.changeObj) {
+              this.changeObj[cellMeta.row][cellMeta.prop] = newValue;
+          } else {
+              // add PK if exists
+              // access data at modified row
+              gridRow = this.data[cellMeta.row];
 
-        // add new change { name : 'test' }
-        dataItem[cellMeta.prop] = newValue;
+              // add new change { name : 'test' }
+              dataItem[cellMeta.prop] = newValue;
 
-        // add primary keys (parent and related) to changeObj at this record
-        // only 0 if pkName not given in recordTypeInfo - shows a validation message
-        if (this.editablePKFieldList.length != 0) {
-          this.editablePKFieldList.forEach((pkField) => {
-            if (pkField in gridRow && cellMeta.prop != pkField) {
-              dataItem[pkField] = gridRow[pkField];
-            }
-          });
-        }
+              // add primary keys (parent and related) to changeObj at this record
+              // only 0 if pkName not given in recordTypeInfo - shows a validation message
+              if (this.editablePKFieldList.length != 0) {
+                  this.editablePKFieldList.forEach(pkField => {
+                  if (pkField in gridRow && cellMeta.prop != pkField) {
+                      dataItem[pkField] = gridRow[pkField];
+                  }
+                  })
+              }
 
-        this.changeObj[cellMeta.row] = dataItem;
+              this.changeObj[cellMeta.row] = dataItem;
+          
+          }
+
       }
-    }
+
   }
 
-    addListeners() {
-        if (this.hotInstance != null) {
-            this.hotInstance.addHook('beforeChange', (changes, source) => {
-                changes?.forEach(change => {
-                  const [row, prop, oldValue, newValue] = change;
-                  if (this.userPermissionLevel === "viewer" ||  this.userPermissionLevel === undefined) {
-                    change[3] = oldValue;
-                  } 
-                })
-              });
-        
-            // Handles saving changes made to the grid by calling onChange and updating changeObj
-            // Sends changeObj to Appian local var in changeData param
-            this.hotInstance.addHook('afterChange', (changes, [source]) => {
-                // call handle change function
-                changes?.forEach(change => {
-                    const [row, prop, oldValue, newValue] = change;
-                    if (newValue != oldValue && this.userPermissionLevel == "editor")
-                    {
-                        let colIdx = this.hotInstance.propToCol(prop);
-                        let cellMeta;
-                        if (colIdx != -1) {
-                            cellMeta = this.hotInstance.getCellMeta(row, colIdx);
-                            this.onChange(cellMeta, newValue);
-                            Appian.Component.saveValue("changeData", Object.values(this.changeObj));
-                        } else {
-                            console.error("Prop not found in column index map");
-                        }
-                    } 
-            
-                });
-            
+  addListeners() {
+      if (this.hotInstance != null) {
+          this.hotInstance.addHook('beforeChange', (changes, source) => {
+              changes?.forEach(change => {
+                const [row, prop, oldValue, newValue] = change;
+                if (this.userPermissionLevel === "viewer" ||  this.userPermissionLevel === undefined) {
+                  change[3] = oldValue;
+                } 
+              })
             });
-            
-        } else {
-            console.error("Hot Instance null");
-        }
-    }
+      
+          // Handles saving changes made to the grid by calling onChange and updating changeObj
+          // Sends changeObj to Appian local var in changeData param
+          this.hotInstance.addHook('afterChange', (changes, [source]) => {
+              // call handle change function
+              changes?.forEach(change => {
+                  const [row, prop, oldValue, newValue] = change;
+                  if (newValue != oldValue && this.userPermissionLevel == "editor")
+                  {
+                      let colIdx = this.hotInstance.propToCol(prop);
+                      let cellMeta;
+                      if (colIdx != -1) {
+                          cellMeta = this.hotInstance.getCellMeta(row, colIdx);
+                          this.onChange(cellMeta, newValue);
+                          Appian.Component.saveValue("changeData", Object.values(this.changeObj));
+                      } else {
+                          console.error("Prop not found in column index map");
+                      }
+                  } 
+          
+              });
+          
+          });
+          
+      } else {
+          console.error("Hot Instance null");
+      }
+  }
 
 }
 
