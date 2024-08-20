a260ff28bd19a8755b97bc6f64a9d328918f8cfb
alex-king10 <alex.king@appian.com>
2024-08-20 17:38:54 -0400

    POC text colConfig

--- servletPlugin/src/main/java/com/appiancorp/cto/editablegrid/function/ColConfigCategory.java
+++ servletPlugin/src/main/java/com/appiancorp/cto/editablegrid/function/ColConfigCategory.java
@@ -1,7 +1,12 @@
 package com.appiancorp.cto.editablegrid.function;
 
+import java.lang.annotation.*;
 import com.appiancorp.suiteapi.expression.annotations.Category;
 
 @Category("ColConfigCategory")
-public class colConfigTypes {
+@Inherited
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface ColConfigCategory {
 }
--- servletPlugin/src/main/java/com/appiancorp/cto/editablegrid/function/TextColConfig.java
+++ servletPlugin/src/main/java/com/appiancorp/cto/editablegrid/function/TextColConfig.java
@@ -1,2 +1,18 @@
-package com.appiancorp.cto.editablegrid.function;public class textColConfig {
+package com.appiancorp.cto.editablegrid.function;
+
+import java.util.HashMap;
+
+import com.appiancorp.suiteapi.expression.annotations.Function;
+import com.appiancorp.suiteapi.expression.annotations.Parameter;
+import org.json.JSONObject;
+@ColConfigCategory
+public class TextColConfig {
+
+  @Function
+  public String textColConfig(@Parameter Boolean readOnly) {
+    System.out.println(readOnly);
+    HashMap result = new HashMap();
+    result.put("readOnly", readOnly);
+    return new JSONObject(result).toString();
+  }
 }
